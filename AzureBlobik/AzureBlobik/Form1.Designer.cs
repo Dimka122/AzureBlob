1257bd972f8964ff2cdc98bedfc614d0ca5b9f75
Дмитрий Кассин <kassindima1@gmail.com>
2023-02-19 07:54:16 +0200

    ///

--- AzureBlobik/AzureBlobik/Form1.Designer.cs
+++ AzureBlobik/AzureBlobik/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 218);
+            this.ClientSize = new System.Drawing.Size(635, 350);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
